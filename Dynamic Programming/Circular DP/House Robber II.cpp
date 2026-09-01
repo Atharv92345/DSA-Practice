d85da43be92e2ce230fac43b2541bd135af1d703
Atharv Tripathi <atharv22531@gmail.com>
2026-09-01 16:55:06 +0530

    House Robber II.cpp

---
+++ Dynamic Programming/Circular DP/House Robber II.cpp
@@ -0,0 +1,26 @@
+//Leetcode 213 - House Robber II
+//https://leetcode.com/problems/house-robber-ii/
+
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        if(nums.size() == 1){
+            return nums[0];
+        }else if(nums.size() == 2){
+            return max(nums[0], nums[1]);
+        }
+        vector<int> dp(nums.size()-1);
+        vector<int> dp2(nums.size()-1);
+        dp[0] = nums[0];
+        dp[1] = max(nums[0], nums[1]);
+        for(int i = 2; i < nums.size()-1; i++){
+            dp[i] = max(dp[i-2] + nums[i], dp[i-1]);
+        }
+        dp2[0] = nums[1];
+        dp2[1] = max(nums[1], nums[2]);
+        for(int i = 3; i < nums.size(); i++){
+            dp2[i-1] = max(dp2[i-3] + nums[i], dp2[i-2]);
+        }
+        return max(dp2[nums.size()-2], dp[nums.size()-2]);
+    }
+};
